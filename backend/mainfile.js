3f480da7ef3ca2666459289e251a0a7e414d9068
vikram <vikrambharathy@gmail.com>
2024-04-13 02:23:40 +0530

    first commit

---
+++ backend/mainfile.js
@@ -0,0 +1,38 @@
+console.log("working");
+const express = require("express");
+const app = express();
+const stripe = require("stripe")("sk_test_51NzuH0SGp41m30vUyUGsnRbqECRBQNafiv6juEhUsYHSpm5CHnQVg8y0jEBSlmr0X9gcyzwwMCJgJsBcxW7hjhgV00Tmfduf8T")
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+        const { products } = req.body;
+
+        const LineItem = products.map((products)=>({
+            price_data: {
+                currency: 'inr',
+                product_data: {
+                    name: products.productname,
+                },
+                unit_amount: products.price*100,
+            },
+            quantity: products.count,
+
+        }))
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: LineItem,
+            mode: 'payment',
+            success_url: 'http://localhost:3000/success',
+            cancel_url: 'http://localhost:3000/cancel',
+        });
+
+        res.json({ id: session.id });
+
+})
+
+app.listen(5000,()=>{
+    console.log("working on port 5000");
+})
